69dd6f7cbe2b3b94bdbc541a89e3282a3374e9af
Lucas dos Santos Camilo <lucasdossantosnet@gmail.com>
2025-04-01 15:49:08 -0300

    Desafio DIO - Trilha Java Básico criando o projeto ContaBanco e executando o desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+
+        // Criando o objeto Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Conta!");
+        int numeroConta = scanner.nextInt();
+        
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String numeroAgencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente da Conta!");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu Saldo da Conta!");
+        double saldoCliente = scanner.nextDouble();
+        
+        // Imprime os dados inseridos pelo usuário
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldoCliente + " já está disponível para saque.");
+
+        // Fecha o Scanner
+        scanner.close();
     }
 }
